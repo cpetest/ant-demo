067361619efd0d53c760d98b442e5cadd62ae0ef
www <wwww>
2019-03-05 11:27:25 +0800

    test

---
+++ src/com/vaannila/domain/UserTest.java
@@ -0,0 +1,27 @@
+package com.vaannila.domain;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class UserTest extends TestCase {
+    int i = 0;
+    Boolean b = false;
+
+    @Test
+    public void test001() {
+        if (i == 0) {
+            b = true;
+        } else {
+            assertTrue("i is 0", b);
+        }
+    }
+
+    @Test
+    public void test002() {
+        if (i == 1) {
+            b = true;
+        } else {
+            assertTrue("i is 0", b);
+        }
+    }
+}
